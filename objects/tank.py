2ebcdb6cb8b80cbe7e0b94b69744004dd67e2790
Phylosius <hei.phylosius@gmail.com>
2024-10-21 17:00:21 +0300

    feat: changed tank image into a nice png image from flaticon.com

--- objects/tank.py
+++ objects/tank.py
@@ -16,13 +16,13 @@ class Tank(Entity, pygame.sprite.Sprite):
         self.rotation_speed = 2  # Plus petit pour des rotations plus fines
         self.radius = 0
 
-        self.image = pygame.Surface([50, 60], pygame.SRCALPHA)
-        self.image.fill("gray")
+        self.image = pygame.image.load('sprites/tank.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
 
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
     def move(self):
-        rad_angle = math.radians(self.radius - 90)
+        rad_angle = math.radians(self.radius + 90)
         self.x += math.cos(rad_angle) * self.speed
         self.y -= math.sin(rad_angle) * self.speed  # Soustraction car l'axe y est inversé
 
