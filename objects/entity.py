89c1964d8439f7939af531405ce0ec863073a9c9
Phylosius <hei.phylosius@gmail.com>
2025-01-30 17:46:49 +0300

    feat: added health system to entity

--- objects/entity.py
+++ objects/entity.py
@@ -4,9 +4,14 @@ class Entity:
 
         self.max_health = 100
         self.health = self.max_health
+        self.is_alive = True
 
         self.x = 50
         self.y = 50
 
         self.speed = 0
 
+    def apply_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.is_alive = False
