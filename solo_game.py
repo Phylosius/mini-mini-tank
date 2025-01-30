31e09399ff4b6efddda69993360097e90f0303f2
Phylosius <hei.phylosius@gmail.com>
2025-01-30 19:00:31 +0300

    feat: SoloGame.handle_pygame_event()

--- solo_game.py
+++ solo_game.py
@@ -11,15 +11,21 @@ class SoloGame(Game):
         self.player = Tank(self, 0, 0)
         self.player_group.add(self.player)
 
+    def handle_pygame_event(self, event):
+
+        # for action to be performed once after pressing
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.player.shoot()
+
     def check_keyboard_press(self):
+        # for action to be performed while pressing
         if self.keys[pygame.K_UP]:
             self.player.move()
         if self.keys[pygame.K_LEFT]:
             self.player.rotate_left()
         if self.keys[pygame.K_RIGHT]:
             self.player.rotate_right()
-        if self.keys[pygame.K_SPACE]:
-            self.player.shoot()
 
     def update(self):
         super().update()
